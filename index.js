d96d8877f92379349157460e9b29abf75b07584c
kevinongg <kevinongg@gmail.comm>
2024-10-23 22:45:39 -0400

    add more code to make fully functional

--- index.js
+++ index.js
@@ -1,73 +1,90 @@
-// step 1: Fetch events
-// assign a variable to the api
-// const API = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2409-FTB-ET-WEB-FT/events`
-// console.log(API)
-
+// step 1: FETCH EVENTS
 // use a function to fetch events and call it later for it to display
 const fetchEvents = async () => {
   // call the events api
   const response = await fetch(`https://fsa-crud-2aa9294fe819.herokuapp.com/api/2409-FTB-ET-WEB-FT/events`);
-  const events = await response.json();
-  console.log(events.name)
-    
+  const responseJson = await response.json();
+  const events = responseJson.data;
+  return events;
+  // console.log(events);
+};
+
+// step 2: DISPLAY EVENTS
+// use a function to display events
+const displayEvents = async (events) => {
+  // create a variable to link fetchEvents function 
+  const eventsList = await fetchEvents();
+  // console.log(eventsList);
+
+  // create an li for each name, date, time, location, description
+  const eventListLIs = eventsList.map((event) => {
+    return `<li>
+      Name:${event.name}<br> 
+      Date:${event.date}<br> 
+      Time:${event.time}<br> 
+      Location:${event.location}<br> 
+      Description:${event.description}
+      </li>`;
+  });
+
+  // grab the ol
+  const OL = document.querySelector(`#event-list`);
+  // append the li to the ul using .join to get rid of the commas
+  OL.innerHTML = eventListLIs.join(``);
 }
-
-fetchEvents()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+displayEvents()
 
 
 // step 3: add new party when submit button is clicked 
-
 // grab the form
 const form = document.querySelector(`form`)
 // add event listener to form when it is submitted
-form.addEventListener(`submit`, async (event) => {
+form.addEventListener(`submit`, (event) => {
   //add event.preventDefault()
   event.preventDefault();
-  // grab the name input
-  const nameInput = document.querySelector(`#event-name`).value
-  // grab the date input
-  const dateInput = document.querySelector(`#event-date`).value
-  // grab the time input
-  const timeInput = document.querySelector(`#event-time`).value
-  // grab the location input
-  const locationInput = document.querySelector(`#event-location`).value
-  // grab the description input
-  const descriptionInput = document.querySelector(`#event-description`).value
-  // call the API with the corresponding value https://fsa-crud-2aa9294fe819.herokuapp.com/api/2409-FTB-ET-WEB-FT/events
-  const response = await fetch(`https://fsa-crud-2aa9294fe819.herokuapp.com/api/2409-FTB-ET-WEB-FT/events`);
-  const eventsData = await response.json();
-  // console.log(eventsData)
+  // grab all inputs from form using an object
+  const userInputEvent = {
+    name: document.querySelector(`#event-name`).value,
+    date: document.querySelector(`#event-date`).value,
+    time: document.querySelector(`#event-time`).value,
+    location: document.querySelector(`#event-location`).value,
+    description: document.querySelector(`#event-description`).value
+  };
 
+  // step 4: add new inputted event to the displayed list
+  // grab the ol
+  const OL = document.querySelector(`#event-list`)
+  // create a new li for each userinput
+  const userInputEventLI = `<li>
+    Name:${userInputEvent.name}<br> 
+    Date:${userInputEvent.date}<br> 
+    Time:${userInputEvent.time}<br> 
+    Location:${userInputEvent.location}<br> 
+    Description:${userInputEvent.description}
+    </li>`;
 
-  // go through the events
-  // creat a new array with just the names of the events
-  const eventsNames = eventsData.name.map((eventsName) => {
-    return eventsName.name;
-  })
-  // create a new li
-  const nameLI = document.createElement(`li`)  
-  // put the name information in the li
-  nameLI.innerHTML = `<h3>${eventsName.name}</h3>`
+  // append the new event to the ol using += (appends new event to existing list without deleting it)
+  OL.innerHTML += userInputEventLI;
 
+  // reset form
+  form.reset();
 })
 
-// // get the value from the input
-// const nameInputValue = nameInput.value
 
+// const displayParties = (parties) => {
+//   const partyList = document.getElementById('party-list');
+//   partyList.innerHTML = ''; // Clear existing list
 
-// const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+// parties.forEach(party => {
+//   const partyDiv = document.createElement('div');
+//   partyDiv.className = 'party';
+//   partyDiv.innerHTML = `
+//       <h3>${party.name}</h3>
+//       <p>Date: ${party.date}</p>
+//       <p>Time: ${party.time}</p>
+//       <p>Location: ${party.location}</p>
+//       <p>Description: ${party.description}</p>
+//   `;
+//   partyList.appendChild(partyDiv);
+// });
+// };
